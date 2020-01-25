0542fe79b83d05c3b1fe73a5c885afae5ea52f3a
eg_b <egbv.py@gmail.com>
2020-01-25 20:26:46 +0300

    small bug fix'

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -10,16 +10,16 @@ def generate_diff(path_to_file1, path_to_file2):
     first_data_set, second_data_set = set(first_data.items()), set(second_data.items())
     intersection = dict(first_data_set.intersection(second_data_set))
     difference = dict(first_data_set.symmetric_difference(second_data_set))
-    answer = '\n'.join('   {}: {}'.format(k, v) for k, v in intersection.items())
+    result = '\n'.join('  {}: {}'.format(k, v) for k, v in intersection.items())
     for k, v in difference.items():
         if k not in second_data.keys():
-            answer += ('\n - ' + '{}: {}'.format(k, v))
+            result += ('\n - ' + '{}: {}'.format(k, v))
         elif k not in first_data.keys():
-            answer += ('\n + ' + '{}: {}'.format(k, v))
+            result += ('\n + ' + '{}: {}'.format(k, v))
         else:
-            answer += ('\n - ' + '{}: {}'.format(k, first_data[k]))
-            answer += ('\n + ' + '{}: {}'.format(k, v))
-    return answer
+            result += ('\n - ' + '{}: {}'.format(k, first_data[k]))
+            result += ('\n + ' + '{}: {}'.format(k, second_data[k]))
+    return '{' + result + '\n}'
 
     
-print(generate_diff('/home/eg/Documents/before.json', '/home/eg/Documents/after.json'))
+#print(generate_diff('/home/eg/Documents/before.json', '/home/eg/Documents/after.json'))
