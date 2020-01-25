331be414423e39c9970370c4f6393d179c722304
eg_b <egbv.py@gmail.com>
2020-01-25 21:54:02 +0300

    gendiff __init__ add

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,14 +3,14 @@ import os
 
 
 def generate_diff(path_to_file1, path_to_file2):
-      
-    first_data, second_data = (json.load(open(os.path.realpath(path_to_file1))),
-                               json.load(open(os.path.realpath(path_to_file2)))
-                            )
-    first_data_set, second_data_set = set(first_data.items()), set(second_data.items())
-    intersection = dict(first_data_set.intersection(second_data_set))
-    difference = dict(first_data_set.symmetric_difference(second_data_set))
-    result = '\n'.join('  {}: {}'.format(k, v) for k, v in intersection.items())
+    first_data, second_data = (
+        json.load(open(os.path.realpath(path_to_file1))),
+        json.load(open(os.path.realpath(path_to_file2)))
+        )
+    first_set, second_set = set(first_data.items()), set(second_data.items())
+    common_data = dict(first_set.intersection(second_set))
+    difference = dict(first_set.symmetric_difference(second_set))
+    result = '\n'.join('  {}: {}'.format(k, v) for k, v in common_data.items())
     for k, v in difference.items():
         if k not in second_data.keys():
             result += ('\n - ' + '{}: {}'.format(k, v))
@@ -20,6 +20,3 @@ def generate_diff(path_to_file1, path_to_file2):
             result += ('\n - ' + '{}: {}'.format(k, first_data[k]))
             result += ('\n + ' + '{}: {}'.format(k, second_data[k]))
     return '{' + result + '\n}'
-
-    
-#print(generate_diff('/home/eg/Documents/before.json', '/home/eg/Documents/after.json'))
