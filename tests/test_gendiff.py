cbad9f7302bedbac98c22cc17df072f36568b4d6
eg_b <egbv.py@gmail.com>
2020-02-07 19:20:14 +0300

    test for CI was fixed

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,15 +5,16 @@ import json, os
 def test_relpath():
     assert generate_diff('fixtures/1.json', 'fixtures/2.json') == '{\n   host: hexlet.io\n - timeout: 50\n}'
 
+
 def test_abspath():
     assert generate_diff(
-        '/home/eg/python_projects/python-project-lvl2/fixtures/2.json',
-        '/home/eg/python_projects/python-project-lvl2/fixtures/1.json'
+        os.path.abspath('./fixtures/2.json'),
+        os.path.abspath('./fixtures/1.json')
     ) == '{\n   host: hexlet.io\n + timeout: 50\n}'
 
 
 def test_both_path():
     assert generate_diff(
         'fixtures/2.json',
-        '/home/eg/python_projects/python-project-lvl2/fixtures/1.json'
+        os.path.abspath('fixtures/1.json')
     ) == '{\n   host: hexlet.io\n + timeout: 50\n}'
