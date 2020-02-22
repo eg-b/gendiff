68eb7777bf1b3f6e030cbe6dda9e30478b89f3f0
eg_b <egbv.py@gmail.com>
2020-02-22 14:32:49 +0300

    compare, generate_diff, parser - refactor

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,29 +1,51 @@
-from gendiff.parsers import parse_files
-
+from gendiff.parsers import parse_paths
+import os, json, yaml
 
 def generate_diff(path_to_file1, path_to_file2):
-    file1_data, file2_data = parse_files(path_to_file1, path_to_file2)
-    intersection, removed, added = make_compare(file1_data, file2_data)
-    result = format_diff(file1_data, file2_data, intersection, removed, added)
+    data_format = parse_paths(path_to_file1, path_to_file2)
+    if data_format == 'json':
+        file1_data, file2_data = (
+            json.load(open(os.path.realpath(path_to_file1))),
+            json.load(open(os.path.realpath(path_to_file2)))
+        )
+    elif data_format == 'yaml':
+        file1_data, file2_data = (
+            (
+                yaml.load(open(os.path.realpath(path_to_file1)),
+                          Loader=yaml.SafeLoader)
+            ),
+            (
+                yaml.load(open(os.path.realpath(path_to_file2)),
+                          Loader=yaml.SafeLoader)
+            )
+        )
+    diff = compare(file1_data, file2_data)
+    result = format_diff(file1_data, file2_data, diff)
     return result
 
 
-def make_compare(file1_data, file2_data):
+def compare(file1_data, file2_data):
     data1_set, data2_set = set(file1_data.keys()), set(file2_data.keys())
-    intersection = data1_set.intersection(data2_set)
+    identical, updated = set(), set()
+    for k in data1_set.intersection(data2_set):
+        if file1_data[k] == file2_data[k]:
+            identical.add(k)
+        else:
+            updated.add(k)
     removed = data1_set.difference(data2_set)
     added = data2_set.difference(data1_set)
-    difference = data1_set.symmetric_difference(data2_set)
-    return intersection, removed, added
+    diff = identical, updated, removed, added
+    return diff
 
 
-def format_diff(file1_data, file2_data, intersection, removed, added):
+def format_diff(file1_data, file2_data, diff):
+    identical, updated, removed, added = diff
     result = ''
-    for k in intersection:
-        if file1_data[k] != file2_data[k]:
-            result += ('\n - ' + '{}: {}'.format(k, file1_data[k]))
-            result += ('\n + ' + '{}: {}'.format(k, file2_data[k]))
-        else: result += ('\n   {}: {}'.format(k, file2_data[k]))
+    for k in identical:
+        result += ('\n   {}: {}'.format(k, file2_data[k]))
+    for k in updated:
+        result += ('\n - ' + '{}: {}'.format(k, file1_data[k]))
+        result += ('\n + ' + '{}: {}'.format(k, file2_data[k]))
     for k in removed:
         result += ('\n - ' + '{}: {}'.format(k, file1_data[k]))
     for k in added:
--- gendiff/parsers.py
+++ gendiff/parsers.py
@@ -1,29 +1,13 @@
-import os
-import json
-import yaml
 
-
-def parse_files(path_to_file1, path_to_file2):
+def parse_paths(path_to_file1, path_to_file2):
     if (
             str(path_to_file1).endswith('.json') and
             str(path_to_file2).endswith('.json')
     ):
-        file1_data, file2_data = (
-            json.load(open(os.path.realpath(path_to_file1))),
-            json.load(open(os.path.realpath(path_to_file2)))
-            )
+        data_format = 'json'
     elif (
             str(path_to_file1).endswith('.yaml') and
             str(path_to_file2).endswith('.yaml')
     ):
-        file1_data, file2_data = (
-            (
-                yaml.load(open(os.path.realpath(path_to_file1)),
-                          Loader=yaml.SafeLoader)
-            ),
-            (
-                yaml.load(open(os.path.realpath(path_to_file2)),
-                          Loader=yaml.SafeLoader)
-            )
-        )
-    return file1_data, file2_data
+        data_format = 'yaml'
+    return data_format
