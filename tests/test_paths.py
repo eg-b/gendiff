77f9f38b7d1fcf83c2131525fa38918222145fa4
eg_b <egbv.py@gmail.com>
2020-04-29 22:38:16 +0300

    paths in tests fixed

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -3,30 +3,31 @@ from gendiff.formatters.plain import render_diff as render
 import os
 
 def test_relpath():
-    assert generate_diff(render, 'fixtures/1.json', 'fixtures/2.json') == "Property 'timeout' was removed"
+    assert generate_diff(render, 'tests/fixtures/1.json', 'tests/fixtures/2.json') == "Property 'timeout' was removed"
+
 
 
 def test_abspath():
     assert generate_diff(render,
-        os.path.abspath('./fixtures/2.yaml'),
-        os.path.abspath('./fixtures/1.yaml')
+        os.path.abspath('tests/fixtures/2.yaml'),
+        os.path.abspath('tests/fixtures/1.yaml')
     ) == "Property 'timeout' was added with value: '50'"
 
 
 def test_abs_and_rel_path():
-    assert generate_diff(render, 'fixtures/2.json', os.path.abspath('fixtures/1.json')
+    assert generate_diff(render, 'tests/fixtures/2.json', os.path.abspath('tests/fixtures/1.json')
     ) == "Property 'timeout' was added with value: '50'"
 
 
 def test_json_and_yaml():
     assert generate_diff(render,
-                         'fixtures/2.yaml',
-                         'fixtures/1.json'
+                         'tests/fixtures/2.yaml',
+                         'tests/fixtures/1.json'
                          ) == "Property 'timeout' was added with value: '50'"
 
 
 def test_yml_and_yaml():
     assert generate_diff(render,
-                         'fixtures/1.yaml',
-                         'fixtures/2.yml'
+                         'tests/fixtures/1.yaml',
+                         'tests/fixtures/2.yml'
                          ) == "Property 'timeout' was removed"
