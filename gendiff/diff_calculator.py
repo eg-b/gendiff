ae654617eacff3dc818e197a6dcc3b0ccb4852fa
eg_b <egbv.py@gmail.com>
2020-05-24 14:51:13 +0300

    jsontxt now work properly

--- gendiff/diff_calculator.py
+++ gendiff/diff_calculator.py
@@ -2,13 +2,9 @@ before = {
   "common": {
     "setting1": "Value 1",
     "setting2": "200",
-    "setting3": 'true',
+    "setting3": True,
     "setting6": {
       "key": "value"
-    },
-    "site": {
-      "base": "hexlet.io",
-      "base2": "hexlet.io"
     }
   },
   "group1": {
@@ -17,23 +13,16 @@ before = {
   },
   "group2": {
     "abc": "12345"
-  },
-  "same": {
-    "abc": "12345",
-    "cba": "2222"
   }
 }
 
 after = {
   "common": {
     "setting1": "Value 1",
-    "setting3": 'true',
+    "setting3": True,
     "setting4": "blah blah",
     "setting5": {
       "key5": "value5"
-    },
-    "site": {
-      "base2": "hexlet.io"
     }
   },
 
@@ -44,11 +33,7 @@ after = {
 
   "group3": {
     "fee": "100500"
-  },
-  "same": {
-    "abc": "12345",
-    "cba": "2222"
-    }
+  }
 }
 
 def compare(file1_data, file2_data):
@@ -57,43 +42,73 @@ def compare(file1_data, file2_data):
     unchanged, updated = set(), set()
     removed = dict.fromkeys(data1_set - data2_set)
     added = dict.fromkeys(data2_set - data1_set)
-
     for k in data1_set & data2_set:
         if file1_data[k] == file2_data[k]:
             unchanged.add(k)
         else:
             if type(file1_data[k]) == dict and type(file2_data[k]) == dict:
                 children = compare(file1_data[k], file2_data[k])
-                diff.update({k + ' changed': children})
+                diff.update({k + '->changed': children})
             else:
                 updated.add(k)
     for k in removed:
-        value = {}
-        if type(file1_data[k]) == dict:
-            for key in file1_data[k]:
-                value.update({f'{key} removed': file1_data[k][key]})
-        else:
-            value = file1_data[k]
-        diff.update({k + ' removed': value})
+        diff.update({k + '->removed': file1_data[k]})
     for k in added:
-        value = {}
-        if type(file2_data[k]) == dict:
-            for key in file2_data[k]:
-                value.update({f'{key} added': file2_data[k][key]})
-        else:
-            value = file2_data[k]
-        diff.update({k + ' added': value})
+        diff.update({k + '->added': file2_data[k]})
     for k in unchanged:
-        value = {}
-        if type(file1_data[k]) == dict:
-            for key in file1_data[k]:
-                value.update({f'{key} unchanged': file1_data[k][key]})
-        else:
-            value = file1_data[k]
-        diff.update({k + ' unchanged': value})
+        diff.update({k + '->unchanged': file2_data[k]})
     for k in updated:
         diff.update(
-            {k + ' changed_from_to': (file1_data[k], file2_data[k])})
+            {k + '->changed_from_to': (file1_data[k], file2_data[k])})
     return diff
 
 print(compare(before, after))
+
+'''
+{'common changed':
+     {'site changed':
+          {'base removed': 'hexlet.io',
+           'base2 unchanged': 'hexlet.io'},
+      'setting2 removed': '200',
+      'setting6 removed':
+          {'key': 'value'},
+      'setting5 added':
+          {'key5': 'value5'},
+      'setting4 added': 'blah blah',
+      'setting1 unchanged': 'Value 1',
+      'setting3 unchanged': 'true'},
+ 'group1 changed':
+     {'foo unchanged': 'bar',
+      'baz changed_from_to': ('bas', 'bars')},
+ 'group2 removed':
+     {'abc': '12345'},
+ 'group3 added':
+  {'fee': '100500'},
+ 'same unchanged':
+  {'abc': '12345', 
+  'cba': '2222'}} 
+'''
+'''
+{'group1->changed':
+     {'foo->unchanged': 'bar',
+      'baz->changed_from_to': ('bas', 'bars')},
+ 'common->changed':
+     {'site->changed':
+          {'base->removed': 'hexlet.io',
+           'base2->unchanged': 'hexlet.io'},
+      'setting6->removed':
+          {'key': 'value'},
+      'setting2->removed': '200',
+      'setting5->added': 
+          {'key5': 'value5'},
+      'setting4->added': 'blah blah',
+      'setting1->unchanged': 'Value 1',
+      'setting3->unchanged': 'true'},
+ 'group2->removed':
+     {'abc': '12345'},
+ 'group3->added':
+     {'fee': '100500'},
+ 'same->unchanged':
+     {'abc': '12345',
+      'cba': '2222'}}
+'''
--- gendiff/formatters/jsontxt.py
+++ gendiff/formatters/jsontxt.py
@@ -1,5 +1,4 @@
-diff = {'common changed': {'site changed': {'base removed': 'hexlet.io', 'base2 unchanged': 'hexlet.io'}, 'setting2 removed': '200', 'setting6 removed': {'key removed': 'value'}, 'setting4 added': 'blah blah', 'setting5 added': {'key5 added': 'value5'}, 'setting1 unchanged': 'Value 1', 'setting3 unchanged': 'true'}, 'group1 changed': {'foo unchanged': 'bar', 'baz changed_from_to': ('bas', 'bars')}, 'group2 removed': {'abc removed': '12345'}, 'group3 added': {'fee added': '100500'}, 'same unchanged': {'abc unchanged': '12345', 'cba unchanged': '2222'}}
-
+diff = {'group1->changed': {'foo->unchanged': 'bar', 'baz->changed_from_to': ('bas', 'bars')}, 'common->changed': {'setting6->removed': {'key': 'value'}, 'setting2->removed': '200', 'setting4->added': 'blah blah', 'setting5->added': {'key5': 'value5'}, 'setting3->unchanged': True, 'setting1->unchanged': 'Value 1'}, 'group2->removed': {'abc': '12345'}, 'group3->added': {'fee': '100500'}}
 
 
 def render_diff(diff, indent_lvl=1):
@@ -7,27 +6,31 @@ def render_diff(diff, indent_lvl=1):
     indent = '    ' * indent_lvl
     indent_minus = indent[:-2] + '- '
     indent_plus = indent[:-2] + '+ '
+    sep = '->'
     for key, value in diff.items():
-        _key = key.split(' ')[0]
-        group = key.split(' ')[1]
-        diff_value = value
-        if type(value) == dict:
-            diff_value = render_diff(value, indent_lvl + 1)
-        new_string = f'{_key}: {diff_value}'
-        if group == 'unchanged':
+        if len(key.split(sep)) == 1:
+            new_string = f'{key}: {value}'
             result.append(indent + new_string)
-        elif group == 'removed':
-            result.append(indent_minus + new_string)
-        elif group == 'added':
-            result.append(indent_plus + new_string)
-        elif group == 'changed':
+        else:
+            _key = key.split(sep)[0]
+            group = key.split(sep)[1]
             if type(value) == dict:
-                diff_value = render_diff(value, indent_lvl + 1)
-            new_string = f'{_key}: {diff_value}'
-            result.append(indent + new_string)
-        elif group == 'changed_from_to':
-            result.append(indent_minus + f'{_key}: {diff_value[0]}')
-            result.append(indent_plus + f'{_key}: {diff_value[1]}')
+                value = render_diff(value, indent_lvl + 1)
+            new_string = f'{_key}: {value}'
+            if group == 'unchanged':
+                result.append(indent + new_string)
+            elif group == 'removed':
+                result.append(indent_minus + new_string)
+            elif group == 'added':
+                result.append(indent_plus + new_string)
+            elif group == 'changed':
+                if type(value) == dict:
+                    value = render_diff(value, indent_lvl + 1)
+                new_string = f'{_key}: {value}'
+                result.append(indent + new_string)
+            elif group == 'changed_from_to':
+                result.append(indent_minus + f'{_key}: {value[0]}')
+                result.append(indent_plus + f'{_key}: {value[1]}')
     return '{' + '\n' + ('\n'.join(result)) + '\n'\
            + '    ' * (indent_lvl - 1) + '}'
 
