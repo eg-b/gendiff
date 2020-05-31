31eec846a010005c64fedc7f336184dadadf1ce1
eg_b <egbv.py@gmail.com>
2020-05-31 14:50:44 +0300

    branch new inner repr

--- gendiff/diff_calculator.py
+++ gendiff/diff_calculator.py
@@ -1,25 +1,79 @@
-def compare(file1_data, file2_data):
+def compare(old_data, new_data):
     diff = {}
-    data1_set, data2_set = file1_data.keys(), file2_data.keys()
-    unchanged, updated = set(), set()
-    removed = dict.fromkeys(data1_set - data2_set)
-    added = dict.fromkeys(data2_set - data1_set)
-    for k in data1_set & data2_set:
-        if file1_data[k] == file2_data[k]:
-            unchanged.add(k)
+    old_keys, new_keys = old_data.keys(), new_data.keys()
+    for k in (old_keys - new_keys):
+        diff.update({k: dict(value=old_data[k], status="removed")})
+    for k in (new_keys - old_keys):
+        diff.update({k: dict(value=new_data[k], status="added")})
+    for k in old_keys & new_keys:
+        if old_data[k] == new_data[k]:
+            diff.update({k: dict(value=new_data[k], status="unchanged")})
         else:
-            if type(file1_data[k]) == dict and type(file2_data[k]) == dict:
-                children = compare(file1_data[k], file2_data[k])
-                diff.update({k + '->changed': children})
+            if type(old_data[k]) == dict and type(new_data[k]) == dict:
+                children = compare(old_data[k], new_data[k])
+                diff.update({k: dict(value='complex_value', status="changed")})
+                diff.update(children)
             else:
-                updated.add(k)
-    for k in removed:
-        diff.update({k + '->removed': file1_data[k]})
-    for k in added:
-        diff.update({k + '->added': file2_data[k]})
-    for k in unchanged:
-        diff.update({k + '->unchanged': file2_data[k]})
-    for k in updated:
-        diff.update(
-            {k + '->changed_from_to': (file1_data[k], file2_data[k])})
+                diff.update({k:dict(old_value=old_data[k],
+                                  new_value=new_data[k],
+                                  status="changed_from_to")})
+    get_keys(diff):
+        for k, v in diff:
+            value = v['value']
+            status = v['status']
+            if type(value) == dict:
+                for key, val in value:
+                    diff.update({key: })
     return diff
+
+
+BEFORE = {
+  "common": {
+    "setting2": "200",
+    "setting3": True,
+    "setting6": {
+      "key": "value"
+    }
+  },
+  "group1": {
+    "baz": "bas",
+    "foo": "bar"
+  },
+  "group2": {
+    "abc": "12345"
+  }
+}
+
+AFTER = {
+  "common": {
+    "setting3": True,
+    "setting4": "blah blah",
+    "setting5": {
+      "key5": "value5"
+    }
+  },
+
+  "group1": {
+    "foo": "bar",
+    "baz": "bars"
+  },
+
+  "group3": {
+    "fee": "100500"
+  }
+}
+
+print(compare(BEFORE, AFTER))
+
+{'group2': {'value': {'abc': '12345'}, 'status': 'removed'},
+ 'group3': {'value': {'fee': '100500'}, 'status': 'added'},
+ 'common': {'value': 'complex_value', 'status': 'changed'},
+ 'setting2': {'value': '200', 'status': 'removed'},
+ 'setting6': {'value': {'key': 'value'}, 'status': 'removed'},
+ 'setting5': {'value': {'key5': 'value5'}, 'status': 'added'},
+ 'setting4': {'value': 'blah blah', 'status': 'added'},
+ 'setting3': {'value': True, 'status': 'unchanged'},
+ 'group1': {'value': 'complex_value', 'status': 'changed'},
+ 'foo': {'value': 'bar', 'status': 'unchanged'},
+ 'baz': {'old_value': 'bas', 'new_value': 'bars', 'status': 'changed_from_to'}}
+
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,10 +1,10 @@
 import argparse
-from gendiff.generate_diff import get_output_format
+from gendiff.formatters import FORMATTERS
 
 
 parser = argparse.ArgumentParser(description='Generate diff')
 parser.add_argument('-f', '--format', action='store',
                     dest='format', metavar='FORMAT', default='jsontxt',
-                    help='set format of output', type=get_output_format)
+                    help='set format of output', type=FORMATTERS.get)
 parser.add_argument('first_file')
 parser.add_argument('second_file')
