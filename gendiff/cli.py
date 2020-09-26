d03456259b99eca7db78c8788582397653ce635e
eg_b <egbv.py@gmail.com>
2020-09-26 10:45:38 +0300

    update help output and README.md

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -5,6 +5,7 @@ from gendiff.formatters import FORMATTERS
 parser = argparse.ArgumentParser(description='Generate diff')
 parser.add_argument('-f', '--format', action='store',
                     dest='format', metavar='FORMAT', default='jsontxt',
-                    help='set format of output', type=FORMATTERS.get)
+                    help='set output format: "jsontxt", "plain", "json"',
+                    type=FORMATTERS.get)
 parser.add_argument('first_file')
 parser.add_argument('second_file')
